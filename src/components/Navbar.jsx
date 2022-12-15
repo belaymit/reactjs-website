ac062c58654fdab5ec3298807548d77c4077973a
belaymit <2belamit@gmail.com>
2022-12-15 20:59:21 +0300

    added update state onClicking logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,22 +7,22 @@ import { links } from '../data';
 import './navbar.css';
 
 const Navbar = () => {
-  const [isNavShowing, setIsNavShowing] = useState(true);
+  const [isNavShowing, setIsNavShowing] = useState(false);
   return (
     <nav className="container nav-container">
-      <Link to="/" className="logo">
+      <Link to="/" className="logo" onClick={() => setIsNavShowing(false)}>
         <img src={Logo} alt="logo" />
       </Link>
       <ul className={`nav-links ${isNavShowing ? 'show-nav' : 'hide-nav'}`}>
         {links.map(({ name, path }, index) => (
         // eslint-disable-next-line react/jsx-key, react/no-array-index-key
           <li key={index}>
-            <NavLink to={path} className={({ isActive }) => (isActive ? 'active-nav' : '')}>{ name }</NavLink>
+            <NavLink to={path} className={({ isActive }) => (isActive ? 'active-nav' : '')} onClick={() => setIsNavShowing((prev) => !prev)}>{ name }</NavLink>
             {' '}
           </li>
         ))}
       </ul>
-      <button type="button" className="nav-toggle-btn" onClick={() => setIsNavShowing(!isNavShowing)}>
+      <button type="button" className="nav-toggle-btn" onClick={() => setIsNavShowing((prev) => !prev)}>
         {isNavShowing ? <MdOutlineClose /> : <GoThreeBars />}
       </button>
     </nav>
