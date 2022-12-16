479b7d05731cea809742caef3ab8df8d11322604
belaymit <2belamit@gmail.com>
2022-12-16 10:59:05 +0300

    created and passed props to Card component

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { FaCrown } from 'react-icons/fa';
 import SectionHead from './SectionHead';
+import { programs } from '../data';
+import Card from '../UI/Card';
 
 const Programs = () => (
   <section className="programs">
     <div className="container program-container">
       <div className="program-head">
         <SectionHead icon={<FaCrown />} title="Programs" />
+      </div>
+      <div className="program-wrapper">
+        {
+          programs.map(({
+            id, icon, title, info,
+          }) => (
+            <Card className="programs-program" key={id}>
+              <span>{icon}</span>
+              <h4>{title}</h4>
+              <small>{info}</small>
+            </Card>
+          ))
+        }
       </div>
     </div>
   </section>
