1a6c8914a06ff22ce742af2d00d03441e399040c
belaymit <2belamit@gmail.com>
2022-12-16 11:06:51 +0300

    added link to programs

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { FaCrown } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { AiFillCaretRight } from 'react-icons/ai';
 import SectionHead from './SectionHead';
 import { programs } from '../data';
 import Card from '../UI/Card';
+import '../Pages/home/home.css';
 
 const Programs = () => (
   <section className="programs">
@@ -13,12 +16,18 @@ const Programs = () => (
       <div className="program-wrapper">
         {
           programs.map(({
-            id, icon, title, info,
+            id, icon, title, info, path,
           }) => (
             <Card className="programs-program" key={id}>
               <span>{icon}</span>
               <h4>{title}</h4>
               <small>{info}</small>
+              <Link to={path} className="btn sm">
+                Learn More
+                {' '}
+                <AiFillCaretRight />
+                {' '}
+              </Link>
             </Card>
           ))
         }
