be3ae7fcddc95a60d8fbddfa2cc93848efd7aed6
belaymit <2belamit@gmail.com>
2022-12-26 13:46:21 +0300

    imported Value component to Home page

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../../components/Header';
 import Programs from '../../components/Programs';
+import Values from '../../components/Values';
 import './home.css';
 
 const Home = () => (
   <>
     <Header />
     <Programs />
+    <Values />
   </>
 
 );
