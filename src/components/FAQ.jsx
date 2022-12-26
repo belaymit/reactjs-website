bd05b82e6a06cb10e55e7657ed190fe67a5fae4c
belaymit <2belamit@gmail.com>
2022-12-26 16:12:21 +0300

    added useState hook to toggle between answer and question in FAQ section

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import { AiOutlinePlus } from 'react-icons/ai';
+import React, { useState } from 'react';
+import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
 
 // eslint-disable-next-line react/prop-types
-const FAQ = ({ question, answer }) => (
-  <article>
-    <div className="faq-question">
-      <h4>{question}</h4>
-      <button className="faq-icon" type="button">
-        <AiOutlinePlus />
-      </button>
-    </div>
-    <p>{answer}</p>
-  </article>
-);
+const FAQ = ({ question, answer }) => {
+  const [isAnswerShowing, setIsAnswerShowing] = useState(false);
+
+  return (
+    // eslint-disable-next-line max-len
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
+    <article className="faq" onClick={() => setIsAnswerShowing((prev) => !prev)}>
+      <div className="faq-question">
+        <h4>{question}</h4>
+        <button className="faq-icon" type="button">
+          {isAnswerShowing ? <AiOutlinePlus /> : <AiOutlineMinus />}
+        </button>
+      </div>
+      { isAnswerShowing && <p>{answer}</p>}
+    </article>
+  );
+};
 
 export default FAQ;
